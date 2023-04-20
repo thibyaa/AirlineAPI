18570ed5338e4c26f78beb398d3b3d88ab72401a
Thibyaa <105393816+thibyaa@users.noreply.github.com>
2023-04-20 15:45:03 +0100

    passenger model completed

---
+++ src/main/java/models/Passenger.java
@@ -0,0 +1,75 @@
+package models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity(name="passengers")
+public class Passenger {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private long id;
+
+    @Column
+    private String name;
+
+    @Column(name="email_address")
+    private String emailAddress;
+
+    @JsonIgnoreProperties({"passengers"})
+    @ManyToMany
+    @JoinTable(
+            name = "passengers_flights",
+            joinColumns = @JoinColumn(name="passenger_id"),
+            inverseJoinColumns = @JoinColumn(name="flight_id")
+    )
+    private List<Flight> flights;
+    public Passenger(String name, String emailAddress){
+        this.name = name;
+        this.emailAddress = emailAddress;
+        this.flights = new ArrayList<>();
+    }
+
+//    EMPTY CONSTRUCTOR
+    public Passenger(){
+
+    }
+
+//    GETTERS AND SETTERS
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public List<Flight> getFlights() {
+        return flights;
+    }
+
+    public void setFlights(List<Flight> flights) {
+        this.flights = flights;
+    }
+}
