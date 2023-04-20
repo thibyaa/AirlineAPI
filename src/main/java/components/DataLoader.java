06548b802c92ac745c415f7bae08df0a094e93d1
Thibyaa <105393816+thibyaa@users.noreply.github.com>
2023-04-20 16:08:21 +0100

    controllers created

--- src/main/java/components/DataLoader.java
+++ src/main/java/components/DataLoader.java
@@ -1,4 +1,22 @@
 package components;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import services.FlightService;
+import services.PassengerService;
+
 public class DataLoader {
+
+    @Autowired
+    PassengerService passengerService;
+
+    @Autowired
+    FlightService flightService;
+
+    public DataLoader(){
+
+    }
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
 }
--- src/main/java/controllers/PassengerController.java
+++ src/main/java/controllers/PassengerController.java
@@ -1,4 +1,15 @@
 package controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/passengers")
 public class PassengerController {
+
+//    TODO: ADD A NEW PASSENGER
+//    TODO: DISPLAY DETAILS OF ALL PASSENGERS
+//    TODO: DISPLAY DETAILS OF SPECIFIC PASSENGERS
+//    TODO: BOOK A PASSENGER ON TO A FLIGHT
+
 }
